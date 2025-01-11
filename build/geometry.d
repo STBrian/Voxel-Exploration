55c92bce2f1aaa570a1a0d48bf60493dc49639ed
STBrian <100104486+STBrian@users.noreply.github.com>
2025-01-11 16:08:58 -0600

    Delete build directory

--- build/geometry.d
+++
@@ -1,4 +0,0 @@
-geometry.o: \
- D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/source/geometry.c \
- D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/include/geometry.h
-D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/include/geometry.h:
--- build/render_utils.d
+++
@@ -1,6 +0,0 @@
-render_utils.o: \
- D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/source/render_utils.c \
- D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/include/render_utils.h \
- D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/include/geometry.h
-D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/include/render_utils.h:
-D:/Users/User/Documentos/CarpetaDeBrian/GitHub/Voxel-Exploration/include/geometry.h:
